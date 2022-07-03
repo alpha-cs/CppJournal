7e5deaf27301980848cfb0e5b799d072d0448099
alpha-cs <alpha@jonathangonzalez.tech>
2022-07-03 02:56:50 -0400

    Initial comit

---
+++ README.cpp
@@ -0,0 +1,30 @@
+/*
+c++ primter 5th edition
+
+This is the home directory of this journal. Within this journal you 
+will find many folder with the format of dates. These dates represents
+when I wrote them i.e when I read the chapter. Becuase I am familiar
+with c++ already I may not right entry for dates I read for the reason
+that they are basic knowledge or I am pretty familiar with what is being
+thought in that chapter/section.
+
+For this reason I will also skip excersis that are to easy. Some might
+be easy but because I am not familiar with them I may still choose to 
+write them for future reference.
+
+I am already pretty familiar with c++09 as it was my first programming
+language I officially learned to use. Some errors in my knowledge include
+jumping to different versions of cpp from c++98 - c++20. This is one of
+the reasons I have decided to relearn cpp.
+
+The main reason why I choose to read c++ primer is because I want to fill
+in the gaps in my knowledge of cpp. I am already familiar with writting
+it but in communicating the language can be a challange. This is because
+I leared to program in cpp in 4 different methods (university professors,
+textbook, youtube, and horrible online homework problems from college).
+
+My goal in reading this book is improve as a programmer. Become a more
+complete and efficient cpp programmer. Be able to communicate the language
+at a higher level. Become a more valuabvle asset to organizations that
+recruiters come to me.
+*/
