368bfcc04a22ac04ae853e16c7d9623f91604129
alpha-cs <alpha@jonathangonzalez.tech>
2022-07-04 01:12:06 -0400

    Sales_item problem

--- 7.3.22/main.cpp
+++ 7.3.22/main.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
+#include "Sales_item.h"
 
 int main(){
-    std::cout << "testing cpp"<< std::endl;
+    Sales_item item1, item2;
+    std::cin >> item1 >> item2;
+    std::cout << item1 + item2 << std::endl;
+
     return 0;
 }
