e01c277243f74d4f5788dd65a318cbf592ce8f25
alpha-cs <alpha@jonathangonzalez.tech>
2022-07-04 02:11:47 -0400

    Chapter 1 Update

--- 7.3.22/main.cpp
+++ 7.3.22/main.cpp
@@ -1,10 +1,31 @@
 #include <iostream>
 #include "Sales_item.h"
 
+
 int main(){
-    Sales_item item1, item2;
-    std::cin >> item1 >> item2;
-    std::cout << item1 + item2 << std::endl;
+    /**
+     * checks if data in buffer
+     * pasrse current transaction with previous transaction
+     * if they are the same book then it adds the total
+     * else outputs total sales, and totalcost of the books
+     * when while condition is complete we need to output the
+     * the remainding buffer. 
+     */
+    Sales_item total;
+    if (std::cin >> total){
+        Sales_item trans;
+        while (std::cin >> trans){
+            if (total.isbn() == trans.isbn()){
+                total += trans;
+            }else{
+                std::cout << total << std::endl;
+                total = trans;
+            }
+        }
+        std::cout << total << std::endl;
+    }else {
+        return -1;
+    }
 
     return 0;
 }
---
+++ 7.3.22/notes.cpp
@@ -0,0 +1,14 @@
+/**
+ * Chapter 1 was the basics of cpp. loops and conditions for failures
+ * 
+ * One of the things that intrigdme in this chapter was the way the of streaming
+ * data from the buffer straight from the function.
+ * Normally I would define these conditions outside the header class but the
+ * Sales_item.h would parse the data as soon as the constructor was created.
+ * The method I was thought was to create the constructor and the call its methods.
+ * Usually I would call the main method of the constructor that would handle everything
+ * handled in the Sales_item class but these method is one I didnt know of.
+ * 
+ * I kinda understand how its parsing data from the buffer as the constructor
+ * but I hope to learn more in chapter 7 as it states in the code.
+ */
