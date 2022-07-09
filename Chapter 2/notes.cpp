d3ba85bba64073493654845c1e4a7008cb4ecc85
alpha-cs <alpha@jonathangonzalez.tech>
2022-07-08 22:21:08 -0400

    Chapter 2

--- Chapter 2/notes.cpp
+++ Chapter 2/notes.cpp
@@ -1,14 +1,43 @@
 /**
- * Chapter 1 was the basics of cpp. loops and conditions for failures
+ * Chapter 2.
+ * Part 1: Chapert 2 seems to be consisting of value types and type
+ * inheratance. Regular, decimal, signed, unsigned type values
+ * and what errors or mistakes can happen if using a combination of them.
  * 
- * One of the things that intrigdme in this chapter was the way the of streaming
- * data from the buffer straight from the function.
- * Normally I would define these conditions outside the header class but the
- * Sales_item.h would parse the data as soon as the constructor was created.
- * The method I was thought was to create the constructor and the call its methods.
- * Usually I would call the main method of the constructor that would handle everything
- * handled in the Sales_item class but these method is one I didnt know of.
+ * Unisgned can never be negative and thus can cause errors when using
+ * it in loops or inheritance with other type value. A common error
+ * that wont output a copiler error is a wrap around due to 
+ * unsigned not entering the negatives and wrapping around.
+ */
+
+/**
+ * I am choosing to skip doing a cpp main function for this lesson as
+ * this chapter main lesson wanted us to define a struct as a temporary
+ * method of defining a class since the book has the not thought that yet.
+ * The lesson was to the same thing as previous chapter but with the struct
+ * and defining the struct to only take 2 isbn input types. No loop for 
+ * adding more books of the same and calculating the total.
+ */
+
+/**
+ * The most important thing I learned in this chapter are #ifndef which basically
+ * are header guards. It is good practive to implement these even though
+ * classes have not been used yet or even, just as good practive. They check
+ * if the header class has been defined if they have then they ignore, if they
+ * havent then they #define the class. End with #endif
  * 
- * I kinda understand how its parsing data from the buffer as the constructor
- * but I hope to learn more in chapter 7 as it states in the code.
+ * This chapter goes into details defining what are pointers, references,
+ * and constants. It showed different implementation, good practices and
+ * common mistackes for all three uses. 
+ * 
+ * I also learned of 2 data types I never used before. "auto" and "decltype()"
+ * which should defin the type automatically. The seem to be pointless for my
+ * implications and needs.
+ * 
+ * Another new thing I learned about are constexpr which is implemented on compile
+ * instead on run. I probably need to learn more about this but the main use for this
+ * that I can tell is for runnning a huge program and having constant values that you need
+ * to do some type of operation on compile time. Since the data they take needs to be
+ * defined, it is pointless adding stream data to them. 
+ * 
  */
